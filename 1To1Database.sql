edf769d49da5ce880f04967e447c18eba1ae7e0c
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-22 15:44:10 +0100

    the first feedback

--- 1To1Database.sql
+++ 1To1Database.sql
@@ -1,10 +1,10 @@
 create database shipsFactory;
 
 use shipsFactory;
-
+-- you must realize the principle "one to one" try to use "unique"
 create table buyers
 (
-buyersId int auto_increment not null,
+buyersId int auto_increment not null, -- change name
 name varchar(100) not null,
 surname varchar(100) not null,
 mobileNumber varchar(12) default 'unknown',
@@ -16,7 +16,7 @@ create table orders
 ordersId int not null,
 buyersId int auto_increment not null,
 shipsType varchar(100) not null,
-shipsColour varchar(100) default 'doesn`t metter',
+shipsColour varchar(100) default 'doesn`t metter', -- enum
 shipsSpeed int not null,
 primary key(ordersId),
 foreign key(buyersId) references buyers(buyersId)
@@ -35,4 +35,6 @@ values
 (234507, 'cyberM3', 'green', 98),
 (560072, 'superSpeedTruk8', 'red', 170),
 (042351, 'mechanicLake2', 'blue', 112);
+
+-- add selects for all tables
 
