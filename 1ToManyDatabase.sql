a7752049d6a82114ad3d4f404ad8f165ad303dcb
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-22 15:50:27 +0100

    the first feedback

--- 1ToManyDatabase.sql
+++ 1ToManyDatabase.sql
@@ -4,19 +4,19 @@ use sheeps;
 
 create table customers
 (
-customersId int auto_increment not null,
+customersId int auto_increment not null, -- id!
 firstName varchar(100) not null,
-secondName varchar(100) not null,
+secondName varchar(100) not null, -- the field should rename to: last_name or surname
 primary key(customersId)
 );
 
 create table sheepsOrders
 (
-sheepsOrdersId int auto_increment not null,
+sheepsOrdersId int auto_increment not null, -- id!
 customersId int not null,
 dateOfOrder dateTime not null,
 primary key(sheepsOrdersId),
-foreign key(customersId) references customers(customersId)
+foreign key(customersId) references customers(customersId) --!
 );
 
 insert into customers
@@ -28,7 +28,7 @@ values
 insert into sheepsOrders
 (customersId, dateOfOrder)
 values
-(1, now()),
+(1, now()), -- use another time
 (2, now()),
 (2, now()),
 (1, now());
